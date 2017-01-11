cc606a5be3d09fea696a19a0486c60b40c961ce2
moozagent <aphinan.pee@gmail.com>
2017-01-11 19:05:55 +0700

    update7

--- app/Http/Controllers/AutoReportNoiseByUserController.php
+++ app/Http/Controllers/AutoReportNoiseByUserController.php
@@ -27,8 +27,8 @@ class AutoReportNoiseByUserController extends Controller
         $store = $res->getBody()->getContents();
         $ans = GuzzleHttp\json_decode($store);
 
-        $noise_area_name =  $ans->results->address_components[3]->short_name;
-        $noise_province_name = $ans->results->address_components[4]->short_name;
+        $noise_area_name =  $ans->results->address_components->short_name;
+        $noise_province_name = $ans->results->address_components->short_name;
         //$ans->results->address_components->short_name[3];
         $data['noise_area_name'] = $noise_area_name;
         //$ans->results->address_components->short_name[4];

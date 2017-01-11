ed9b21a85b4fe46497402f613403501bb594c9ac
moozagent <aphinan.pee@gmail.com>
2017-01-11 09:05:59 +0700

    update task

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,8 +24,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->call('\App\Http\Controllers\WeatherStationController@callData')
-        ->hourly();
+        $schedule->call('\App\Http\Controllers\WeatherStationController@callData');
     }
 
     /**

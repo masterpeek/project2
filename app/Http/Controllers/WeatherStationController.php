1b2896a17e31febed61d100b6b2bea2084173e59
moozagent <aphinan.pee@gmail.com>
2017-01-13 13:15:42 +0700

    new update61

--- app/Http/Controllers/WeatherStationController.php
+++ app/Http/Controllers/WeatherStationController.php
@@ -74,8 +74,8 @@ class WeatherStationController extends Controller
         $lat = $data["Latitude"];
         $lng = $data["Longitude"];
 
-        $ans = WeatherStation::select(
-                DB::raw('(3959 * acos(cos(radians(' . $lat . ')) * cos(radians(lat)) 
+        $ans = DB::select(
+            ('SELECT * , (3959 * acos(cos(radians(' . $lat . ')) * cos(radians(lat)) 
         * cos(radians(long ) - radians(' . $lng . ')) 
         + sin(radians(' . $lat .')) * sin(radians(lat)))) as distance'))
             ->orderBy('distance')

d9823f070503d34c3bd69655e858a6bac8e5ace2
moozagent <aphinan.pee@gmail.com>
2017-01-19 11:21:27 +0700

    new update 167

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -16,7 +16,7 @@
         }
         .demo-card-square > .mdl-card__title {
             background: url('../station.jpg');
-            background-size: 270px;
+            background-size: cover;
             background-position: center;
 
         }

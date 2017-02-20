cd6837b07934e81c805c8bcbc1ebf845bbd4c9b9
moozagent <aphinan.pee@gmail.com>
2017-02-20 21:11:16 +0700

    update7

--- app/Http/Controllers/AutoReportNoiseByUserController.php
+++ app/Http/Controllers/AutoReportNoiseByUserController.php
@@ -36,8 +36,8 @@ class AutoReportNoiseByUserController extends Controller
         $noise_area_name =  explode(",", $ans->results[0]->formatted_address);
         //$noise_province_name = $ans->results[5]->address_components[0]->long_name;
         $noise_province_name1 = explode(",", $ans->results[3]->formatted_address);
-        $noise_province_name2 = explode("Chang Wat", $noise_province_name1);
-        $noise_province_name3 = explode("/[^0-9]*([0-9]+)[^0-9]*/", $noise_province_name2);
+        $noise_province_name2 = explode("Chang Wat", $noise_province_name1[0]);
+        $noise_province_name3 = explode("/[^0-9]*([0-9]+)[^0-9]*/", $noise_province_name2[0]);
 
         $area1 = $tr->setSource('en')->setTarget('th')->translate($noise_area_name[1]);
         $area2 = $tr->setSource('en')->setTarget('th')->translate($noise_area_name[2]);

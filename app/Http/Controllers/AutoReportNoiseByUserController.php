76d41ef28b976ef346a6d18494f6353d5cbd8d4d
moozagent <aphinan.pee@gmail.com>
2017-02-20 21:50:09 +0700

    update13

--- app/Http/Controllers/AutoReportNoiseByUserController.php
+++ app/Http/Controllers/AutoReportNoiseByUserController.php
@@ -38,21 +38,14 @@ class AutoReportNoiseByUserController extends Controller
         $noise_province_name1 = explode(",", $ans->results[3]->formatted_address);
         $noise_province_name2 = explode("Chang Wat", $noise_province_name1[0]);
 
-        str_replace("0","",$noise_province_name2[0]);
-        str_replace("1","",$noise_province_name2[0]);
-        str_replace("2","",$noise_province_name2[0]);
-        str_replace("3","",$noise_province_name2[0]);
-        str_replace("4","",$noise_province_name2[0]);
-        str_replace("5","",$noise_province_name2[0]);
-        str_replace("6","",$noise_province_name2[0]);
-        str_replace("7","",$noise_province_name2[0]);
-        str_replace("8","",$noise_province_name2[0]);
-        str_replace("9","",$noise_province_name2[0]);
-        
+        $number = array("0", "1", "2", "3", "4", "5", "6", "7", "8", "9");
+
+        $noise_province_name3 = str_replace($number,"",$noise_province_name2[0]);
+
         $area1 = $tr->setSource('en')->setTarget('th')->translate($noise_area_name[1]);
         $area2 = $tr->setSource('en')->setTarget('th')->translate($noise_area_name[2]);
 
-        $province1 = $tr->setSource('en')->setTarget('th')->translate($noise_province_name2[0]);
+        $province1 = $tr->setSource('en')->setTarget('th')->translate($noise_province_name3[0]);
 
         $data['noise_area_name'] = $area1." ".$area2;
 

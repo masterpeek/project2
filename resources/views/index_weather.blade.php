9ce3bf7c72599452b208518351695fa8c43fc993
moozagent <aphinan.pee@gmail.com>
2017-02-23 13:53:42 +0700

    updat45

--- resources/views/index_weather.blade.php
+++ resources/views/index_weather.blade.php
@@ -22,8 +22,7 @@
                 <div class="mdl-cell mdl-cell--3-col">
                     <div class="demo-card-square mdl-card mdl-shadow--2dp">
                         <div class="mdl-card__title mdl-card--expand">
-                            <h4 class="mdl-card__title-text">ค่า AQI: {{ $data->aqi_value }} <br>
-                                ระดับคุณภาพอากาศ: {{ $data->aqi_condition_name }}</h4>
+                            <h2 class="mdl-card__title-text">ระดับคุณภาพอากาศ: {{ $data->aqi_condition_name }}</h2>
 
                         </div>
                         <div class="mdl-card__supporting-text">

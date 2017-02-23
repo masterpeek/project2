33d6b75a0d2f33b69b2f1237e7cb7e8966dc848e
moozagent <aphinan.pee@gmail.com>
2017-02-23 13:29:32 +0700

    updat31

--- resources/views/index_weather.blade.php
+++ resources/views/index_weather.blade.php
@@ -21,7 +21,7 @@
             @foreach($datas as $data)
                 <div class="mdl-cell mdl-cell--4-col">
                     <div class="demo-card-square mdl-card mdl-shadow--2dp">
-                        <div class="mdl-card__title mdl-card--expand">
+                        {{ $data->aqi_value }}
                             <h2 class="mdl-card__title-text"></h2>
                         </div>
                         <div class="mdl-card__supporting-text">

4a5f12f4f9d778a2fe3c7b8fdffc815f19a3d1a6
moozagent <aphinan.pee@gmail.com>
2017-02-23 15:03:53 +0700

    update63

--- app/Http/Controllers/WebController.php
+++ app/Http/Controllers/WebController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\WeatherStation;
 
 class WebController extends Controller
 {
@@ -11,7 +12,7 @@ class WebController extends Controller
         $markers = WeatherStation::all();
 
         return view('index')->with('markers', $markers);
-        
+
     }
 
     public function about()

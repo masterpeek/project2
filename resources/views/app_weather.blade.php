ace4db964d7ad0211120e2d26730358b5ec549b4
moozagent <aphinan.pee@gmail.com>
2017-02-24 10:31:10 +0700

    updat88

--- resources/views/app_weather.blade.php
+++ resources/views/app_weather.blade.php
@@ -8,7 +8,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <style>
         .demo-card-square.mdl-card {
-            width: 103%;
+            width: 102%;
             height: 33%;
             margin-left: 0%;
             margin-right: 0%;
@@ -79,9 +79,6 @@
 
 
     </main>
-    <footer class="mdl-mini-footer">
-
-    </footer>
 </div>
 </body>
 </html>

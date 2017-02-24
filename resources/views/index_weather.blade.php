286f2679b4ae76a59e30f1ea47061d1a5ecff2b7
moozagent <aphinan.pee@gmail.com>
2017-02-24 10:12:57 +0700

    updat82

--- resources/views/index_weather.blade.php
+++ resources/views/index_weather.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <section class="section--center mdl-grid mdl-grid--no-spacing mdl-shadow--2dp">
-        <div class="mdl-grid" style="max-height: 200%">
+        <div class="mdl-grid" style="max-height: 1000px">
             <br>
             {!! Form::open(['url' => 'search_weather', 'method' => 'post']) !!}
             <div class="row">

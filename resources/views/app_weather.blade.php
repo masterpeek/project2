bf24ca8ee81f7faa765c9c547fb3a857946f7bc6
moozagent <aphinan.pee@gmail.com>
2017-02-24 10:19:32 +0700

    updat84

--- resources/views/app_weather.blade.php
+++ resources/views/app_weather.blade.php
@@ -8,12 +8,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <style>
         .demo-card-square.mdl-card {
-            width: 100%;
-            height: 35%;
-            margin-left: 2%;
+            width: 105%;
+            height: 30%;
+            margin-left: 1%;
             margin-right: 2%;
-            margin-bottom: 2%;
-            margin-top: 2%;
+            margin-bottom: 1%;
+            margin-top: 1%;
 
         }
         .demo-card-square > .mdl-card__title {

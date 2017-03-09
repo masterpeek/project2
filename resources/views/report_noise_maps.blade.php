d6082b1d284f439e5b8212b41fd2222c06aca2d6
moozagent <aphinan.pee@gmail.com>
2017-03-09 14:26:47 +0700

    update57

--- resources/views/report_noise_maps.blade.php
+++ resources/views/report_noise_maps.blade.php
@@ -65,7 +65,7 @@
 </script>
 <br>
 <div class="form-group" style="text-align:center">
-    <img src="noise_color.png" height="100" width="700">
+    <img src="noise_color.png" height="100" width="750">
 </div>
 </body>
 </html>

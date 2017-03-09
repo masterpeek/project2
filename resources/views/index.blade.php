e1d8d97f796f39babd3d3d1e5c7cd1d43a0631f3
moozagent <aphinan.pee@gmail.com>
2017-03-09 11:06:47 +0700

    update34

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -204,6 +204,7 @@
         src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDSolnKvQzksYqxOviOJrNTkRn7-voF9MA&callback=initMap">
 </script>
 <br><br><br>
+<img src="show_color" height="400" width="300">
 </body>
 </html>
 @stop

15b8018767e5afbfe82ec9c3771daec6df6a0996
moozagent <aphinan.pee@gmail.com>
2017-03-09 10:57:11 +0700

    update33

--- resources/views/show_report_air.blade.php
+++ resources/views/show_report_air.blade.php
@@ -47,15 +47,15 @@
 
                 if(value == "เล็กน้อย")
                 {
-                    marker.setIcon('cloud1.png');
+                    marker.setIcon('http://maps.google.com/mapfiles/ms/icons/yellow-dot.png');
                 }
                 else if(value == "ปานกลาง")
                 {
-                    marker.setIcon('cloud2.png');
+                    marker.setIcon('http://maps.google.com/mapfiles/ms/icons/orange-dot.png');
                 }
                 else if(value == "รุนแรง")
                 {
-                    marker.setIcon('cloud3.png');
+                    marker.setIcon('http://maps.google.com/mapfiles/ms/icons/red-dot.png');
                 }
 
                 google.maps.event.addListener(marker, 'click', function() {

42cc09410d5ca28a7ba09f27bb8281fde25d4495
moozagent <aphinan.pee@gmail.com>
2017-04-01 00:16:40 +0700

    update friday46

--- resources/views/index_admin_edit.blade.php
+++ resources/views/index_admin_edit.blade.php
@@ -15,7 +15,7 @@
             <div class="col-sm-9">
     <div class="container">
         <div class="row">
-            <div class="col-md-4 col-md-offset-3">
+            <div class="col-md-4 col-md-offset-2">
                 <div class="panel panel-default">
                     <div class="panel-heading">
                         <h1 class="panel-title" align="center">สมัครสมาชิก</h1>

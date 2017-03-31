11667d2d2917191340e351273f033c532dca1bdf
moozagent <aphinan.pee@gmail.com>
2017-03-31 20:07:42 +0700

    update friday5

--- resources/views/create_form_admin.blade.php
+++ resources/views/create_form_admin.blade.php
@@ -1,14 +1,14 @@
 @extends('app_admin')
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-4 col-md-offset-4">
-            <div class="panel panel-default">
-                <div class="panel-heading">
-                    <h1 class="panel-title" align="center">สมัครสมาชิก</h1>
-                </div>
-                <div class="panel-body">
-                    {!! Form::open(['url' => 'create_admin', 'method' => 'post']) !!}
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4 col-md-offset-4">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <h1 class="panel-title" align="center">สมัครสมาชิก</h1>
+                    </div>
+                    <div class="panel-body">
+                        {!! Form::open(['url' => 'create_admin', 'method' => 'post']) !!}
                         <fieldset>
                             <div class="form-group">
                                 <input class="form-control" placeholder="ชื่อผู้ใช้" type="text" name="Username">
@@ -30,10 +30,10 @@
                             </div>
                             <input class="btn btn-lg btn-success btn-block" type="submit" value="ยืนยัน">
                         </fieldset>
-                    {!! Form::close() !!}
+                        {!! Form::close() !!}
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
-    @stop
+@stop

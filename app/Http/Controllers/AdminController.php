9ba66b51125791978ae48a787ae08d2afc5b16b6
moozagent <aphinan.pee@gmail.com>
2017-03-31 20:02:38 +0700

    update friday3

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -10,7 +10,7 @@ class AdminController extends Controller
 {
     public function index()
     {
-        return view('/index_admin');
+        return view('/login_admin');
     }
 
     public function create()

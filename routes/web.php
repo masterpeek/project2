8793bd97dcced91f7cef7752bfa14d0253d817d6
moozagent <aphinan.pee@gmail.com>
2017-03-31 20:18:36 +0700

    update friday7

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,7 @@ Route::post('/search_all', 'WebController@search_all');
 Route::get('/login_admin', 'AdminController@index');
 Route::get('/create_form_admin', 'AdminController@create');
 Route::post('/create_admin', 'AdminController@createAdmin');
-Route::get('/check_login_admin', 'AdminController@loginAdmin');
+Route::post('/check_login_admin', 'AdminController@loginAdmin');
 
 Route::get('call_data_weather_station', 'WeatherStationController@callData');
 Route::get('return_data_weather_station', 'WeatherStationController@allData');

f49725bd9a81c89f919aef21b4858be92b86166e
moozagent <aphinan.pee@gmail.com>
2017-04-02 00:42:00 +0700

    update satุ

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -7,6 +7,7 @@ use App\WeatherStation;
 use App\AutoReportNoiseByUser;
 use App\ReportAirByUser;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Input;
 use Illuminate\Support\Facades\Redirect;
 use Illuminate\Support\Facades\DB;
 use Validator;

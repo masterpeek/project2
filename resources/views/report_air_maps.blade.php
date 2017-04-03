9aa079a8dcfc50db719c83810cac5e61376a5b7f
moozagent <aphinan.pee@gmail.com>
2017-04-04 00:30:48 +0700

    update bug air5

--- resources/views/report_air_maps.blade.php
+++ resources/views/report_air_maps.blade.php
@@ -41,7 +41,7 @@
                 @foreach($markers as $marker)
             ['', {{ $marker->air_lat }}, {{ $marker->air_long }}, "{{ $marker->air_pollution }}",
                 "{{ $marker->air_area_name }}", "{{ $marker->air_province_name }}", "{{ $marker->air_thai_date }}",
-            "{{ $marker->air_picture }}"],
+            "{{ base64_decode($marker->air_picture) }}"],
             @endforeach
         ];
 
@@ -56,7 +56,7 @@
 
             var content = "มลพิษทางอากาศ: "+ value +
                 "<br>" + "พื้นที่: "+ area +" "+ province + "<br>" +
-                "วันที่: "+ date + "<br><img align='center' src='data:image/jpeg;base64,"+ pic + "'/>";
+                "วันที่: "+ date + "<br><img src='data:image/jpeg;base64,"+ pic + "'/>";
 
             var infowindow = new google.maps.InfoWindow({
                 content: content

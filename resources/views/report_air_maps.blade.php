8682bc0a7e45f2be399e0b3bfdb249e7a9a3ff96
moozagent <aphinan.pee@gmail.com>
2017-04-03 16:53:35 +0700

    update fix bug air noise12

--- resources/views/report_air_maps.blade.php
+++ resources/views/report_air_maps.blade.php
@@ -39,7 +39,7 @@
         var markers = [
                 @foreach($markers as $marker)
             ['', {{ $marker->air_lat }}, {{ $marker->air_long }}, "{{ $marker->air_pollution }}",
-                "{{ $marker->air_area_name }}", "{{ $marker->air_province_name }}", "{{ $marker->air_thai_date }}", "{{ $marker->air_picture }}"],
+                "{{ $marker->air_area_name }}", "{{ $marker->air_province_name }}", "{{ $marker->air_thai_date }}"],
             @endforeach
         ];
 

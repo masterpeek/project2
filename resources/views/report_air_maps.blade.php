b39afa5818e6d5b7ccc10f16816fe826f9f1a94f
moozagent <aphinan.pee@gmail.com>
2017-04-04 16:32:48 +0700

    update bug air24

--- resources/views/report_air_maps.blade.php
+++ resources/views/report_air_maps.blade.php
@@ -40,9 +40,16 @@
         var markers = [
                 @foreach($markers as $marker)
             ['', {{ $marker->air_lat }}, {{ $marker->air_long }}, "{{ $marker->air_pollution }}",
-                "{{ $marker->air_area_name }}", "{{ $marker->air_province_name }}", "{{ $marker->air_thai_date }}", "{{ $marker->air_picture }}"],
+                "{{ $marker->air_area_name }}", "{{ $marker->air_province_name }}", "{{ $marker->air_thai_date }}"],
             @endforeach
         ];
+
+        var pictures =
+            [
+                    @foreach($markers as $marker)
+                ['', "{{ $marker->air_picture }}"],
+                @endforeach
+            ];
 
         for (i = 0; i < markers.length; i++) {
             var position = new google.maps.LatLng(markers[i][1], markers[i][2]);
@@ -51,7 +58,7 @@
             var area = markers[i][4];
             var province = markers[i][5];
             var date = markers[i][6];
-            var pic = markers[i][7];
+            var pic = pictures[i][7];
 
             var content = "มลพิษทางอากาศ: "+ value +
                 "<br>" + "พื้นที่: "+ area +" "+ province + "<br>" +

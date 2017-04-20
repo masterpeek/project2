a47390649b2e492444b9ffed5fcbd72117fece84
moozagent <aphinan.pee@gmail.com>
2017-04-20 11:15:33 +0700

    update Thurs24

--- resources/views/show_air_picture.blade.php
+++ resources/views/show_air_picture.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 
     <div class="container" style="text-align: center">
-        <img src="data:image/jpeg;base64,{{ $air_picture->air_picture }}" class="centered" width="600" height="600">
+        <img src="data:image/jpeg;base64,{{ $air_picture->air_picture }}" class="centered" width="500" height="500">
     </div>
 
     @stop

48c0184998150310ccaea6861c3a22784c9c70c2
moozagent <aphinan.pee@gmail.com>
2017-04-20 11:14:14 +0700

    update Thurs23

--- resources/views/show_air_picture.blade.php
+++ resources/views/show_air_picture.blade.php
@@ -1,14 +1,10 @@
 @extends('app_air_maps')
 
 @section('content')
-    <section class="section--center mdl-grid mdl-grid--no-spacing mdl-shadow--2dp">
-        <div class="mdl-grid">
-
 
     <div class="container" style="text-align: center">
-        <img src="data:image/jpeg;base64,{{ $air_picture->air_picture }}" class="centered" width="400" height="200">
+        <img src="data:image/jpeg;base64,{{ $air_picture->air_picture }}" class="centered" width="600" height="600">
     </div>
-        </div>
-    </section>
+
     @stop
 

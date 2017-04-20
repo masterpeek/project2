7d3a95991ce1b834f2f8a95db7b21e18515295ad
moozagent <aphinan.pee@gmail.com>
2017-04-20 10:46:59 +0700

    update Thurs13

--- resources/views/show_air_picture.blade.php
+++ resources/views/show_air_picture.blade.php
@@ -2,7 +2,7 @@
 @section('content')
 
 
-    <div class="container" style="margin:0px auto; text-align:center;">
+    <div class="container" style="border: 1px solid #000; height: 600px; width: 600px; line-height: 600px; *font-size: 600px; text-align: center;">
         <img src="data:image/jpeg;base64,{{ $air_picture->air_picture }}" class="img-rounded" width="304" height="236">
         <br>
         <a href="{{ url('report_air_maps') }}">ย้อนกลับ</a>

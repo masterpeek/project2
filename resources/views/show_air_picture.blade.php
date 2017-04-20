0bee05f2b84caf32fa0176986ae615405d4c2fb5
moozagent <aphinan.pee@gmail.com>
2017-04-20 10:49:22 +0700

    update Thurs15

--- resources/views/show_air_picture.blade.php
+++ resources/views/show_air_picture.blade.php
@@ -4,8 +4,8 @@
 
     <div class="container" style="height: 600px; width: 600px; line-height: 600px; *font-size: 600px; text-align: center;">
         <img src="data:image/jpeg;base64,{{ $air_picture->air_picture }}" class="img-rounded" width="304" height="236">
-        <br>
-        <a href="{{ url('report_air_maps') }}">ย้อนกลับ</a>
     </div>
+    
+    <a href="{{ url('report_air_maps') }}">ย้อนกลับ</a>
 
     @stop

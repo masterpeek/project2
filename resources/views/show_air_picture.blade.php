3f204fd7a3d858e32a4e000298965dfb648f2d1d
moozagent <aphinan.pee@gmail.com>
2017-04-20 11:31:19 +0700

    update Thurs30

--- resources/views/show_air_picture.blade.php
+++ resources/views/show_air_picture.blade.php
@@ -6,6 +6,8 @@
         <img src="data:image/jpeg;base64,{{ $air_picture->air_picture }}" class="centered" width="500" height="500">
     </div>
     <br>
-    <a href="{{ url('show_report_air') }}" class="btn btn-primary" role="button">กลับสู่หน้าแผนที่มลพิษทางอากาศ</a>
+    <div class="form-group" style="text-align:center">
+    <a href="{{ url('show_report_air') }}" class="btn btn-primary" role="button">กลับสู่หน้าแผนที่</a>
+    </div>
     @stop
 

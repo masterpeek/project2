1065941b71ccab2d118e91b7d5ff4a2523f975b0
moozagent <aphinan.pee@gmail.com>
2017-04-20 11:32:28 +0700

    update Thurs31

--- resources/views/show_air_picture.blade.php
+++ resources/views/show_air_picture.blade.php
@@ -3,11 +3,11 @@
 @section('content')
     <br>
     <div class="form-group" style="text-align:center">
-        <img src="data:image/jpeg;base64,{{ $air_picture->air_picture }}" class="centered" width="500" height="500">
+        <img src="data:image/jpeg;base64,{{ $air_picture->air_picture }}" class="centered" width="350" height="350">
     </div>
     <br>
     <div class="form-group" style="text-align:center">
-    <a href="{{ url('show_report_air') }}" class="btn btn-primary" role="button">กลับสู่หน้าแผนที่</a>
+    <a href="{{ url('report_air_maps') }}" class="btn btn-primary" role="button">กลับสู่หน้าแผนที่</a>
     </div>
     @stop
 

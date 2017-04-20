245bd86f88d0214389249a7d5d15cd368bc749cf
moozagent <aphinan.pee@gmail.com>
2017-04-20 10:52:27 +0700

    update Thurs17

--- resources/views/show_air_picture.blade.php
+++ resources/views/show_air_picture.blade.php
@@ -2,7 +2,7 @@
 @section('content')
 
 
-    <div class="container" style="height: 600px; width: 600px; line-height: 600px; *font-size: 600px; text-align: center;">
+    <div class="container" style="display: block;margin-left: auto;margin-right: auto">
         <img src="data:image/jpeg;base64,{{ $air_picture->air_picture }}" class="img-rounded" width="304" height="236">
     </div>
 

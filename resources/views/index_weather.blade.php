269bdcec7f137300278f06a91ef73c255d58192d
masterpeek <aphinan.pee@gmail.com>
2017-09-05 09:33:26 +0700

    fix bug index and maps weather

--- resources/views/index_weather.blade.php
+++ resources/views/index_weather.blade.php
@@ -29,7 +29,7 @@
                         <option value="ปานกลาง">คุณภาพปานกลาง</option>
                         <option value="กระทบต่อสุขภาพ">กระทบต่อสุขภาพ</option>
                         <option value="กระทบต่อสุขภาพมาก">กระทบต่อสุขภาพมาก</option>
-                        <option value="อันตราย">รุนแรง</option>
+                        <option value="อันตราย">อันตราย</option>
                     </select>
                     <input type="submit" value="ค้นหา">
                 </div>
@@ -48,6 +48,7 @@
                                         <div class="mdl-card__title mdl-card--expand" style="background: #f7ca18">
                                             @elseif($data->aqi_value >= 201 && $data->aqi_value <= 300)
                                                 <div class="mdl-card__title mdl-card--expand" style="background: #ff9900">
+                                                    @elseif($data->aqi_value > 300)
                                     @endif
                             <h2 class="mdl-card__title-text" style="color: #ffffff">ค่า AQI: {{ $data->aqi_value }} <br> คุณภาพ: {{ $data->aqi_condition_name }} </h2>
 
